0fec8e78727d1c33609894d892657aa5cba0817a
zill-mint <zillurdeu@gmail.com>
2026-09-17 17:15:13 +0200

    add: logger

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TicketsController } from './tickets.controller.js';
 import { TicketsService } from './tickets.service.js';
+import { RequestLoggerMiddleware } from '../common/request-logger.middleware.js';
 
 @Module({
   controllers: [TicketsController],
   providers: [TicketsService]
 })
-export class TicketsModule {}
+export class TicketsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes(TicketsController);
+  }
+}
