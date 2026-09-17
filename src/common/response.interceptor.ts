a82b2ec615681d49e9e83346fe0bae4e2add31f9
zill-mint <zillurdeu@gmail.com>
2026-09-17 18:07:13 +0200

    add: interceptor for making response alike

---
+++ src/common/response.interceptor.ts
@@ -0,0 +1,14 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        data
+      }))
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './common/response.interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   )
+
+  app.useGlobalInterceptors(new ResponseInterceptor);
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
